597b2775390a4d7ff0aca108241b39b80b53c151
Hamm <majhamm@qq.com>
2020-11-20 16:59:37 +0800

    rm: 删除部分注释的代码

--- src/components/SearchSongs.vue
+++ src/components/SearchSongs.vue
@@ -4,7 +4,6 @@
       <div class="bbbug_main_right_song">
         <div class="bbbug_main_right_song_title">
           歌曲搜索
-          <!-- <router-link to="/search_songs" class="bbbug_main_right_song_right">我要点歌</router-link> -->
           <el-autocomplete @select="handleSelect" :fetch-suggestions="querySearch"
             style="margin-top: 20px;display:block;" placeholder="歌名/歌手/专辑搜索" size="small" v-model="keyword" clearable
             @keydown.13.native="getList">
