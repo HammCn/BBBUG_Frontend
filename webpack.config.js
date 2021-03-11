1ef2f97adf568446f6779cfa085586025ede447c
Hamm <majhamm@qq.com>
2021-03-11 15:14:18 +0800

    rm: 移除SourceMap

--- webpack.config.js
+++ webpack.config.js
@@ -76,5 +76,6 @@ module.exports = (options = {}) => ({
       index: url.parse(options.dev ? '/assets/' : publicPath).pathname
     }
   },
-  devtool: options.dev ? '#eval-source-map' : '#source-map'
+  // devtool: options.dev ? '#eval-source-map' : '#source-map'
+  devtool: 'cheap-module-source-map'
 })
