f6379da1c29a85a44b7c36c77d322d94980f0062
fei <1491180173@qq.com>
2021-07-03 15:05:50 +0800

    feat: 登录校验账号

--- webpack.config.js
+++ webpack.config.js
@@ -55,7 +55,7 @@ module.exports = (options = {}) => ({
   ],
   resolve: {
     alias: {
-      '~': resolve(__dirname, 'src')
+      '@': resolve(__dirname, 'src'),
     },
     extensions: ['.js', '.vue', '.json', '.css']
   },
